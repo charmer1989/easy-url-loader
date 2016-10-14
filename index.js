070527dacca1f5238ca1a167e46cf4a98a059c10
Lux <lux@LuxdeMac-mini.local>
2016-10-14 15:00:05 +0800

    cdn

--- index.js
+++ index.js
@@ -3,28 +3,22 @@ var mime = require("mime");
 var fileLoader = require("file-loader");
 var isDev = process.env.NODE_ENV !== 'production';
 module.exports = function(content) {
-        this.cacheable && this.cacheable();
-        var query = loaderUtils.parseQuery(this.query);
-        var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
-    if(query.limit) {
-                limit = parseInt(query.limit, 10);
-
+    this.cacheable && this.cacheable();
+    var query = loaderUtils.parseQuery(this.query);
+    var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
+    if (query.limit) {
+        limit = parseInt(query.limit, 10);
     }
-        var mimetype = query.mimetype || query.minetype || mime.lookup(this.resourcePath);
-    if(limit <= 0 || content.length < limit) {
-                return "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64"));
-
+    var mimetype = query.mimetype || query.minetype || mime.lookup(this.resourcePath);
+    if (limit <= 0 || content.length < limit) {
+        return "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64"));
     } else {
-                var res = fileLoader.call(this, content);
-        if(/\"([a-f0-9]{32})(\.png)/.exec(res)[1].length===32&&!isDev){
-                        return res.replace(/\"([a-f0-9]{32})(\.png)/, '\"public/dist/$1$2');
-
+        var res = fileLoader.call(this, content);
+        if (/\"([a-f0-9]{32})(\.png)/.exec(res)[1].length === 32 && !isDev) {
+            return res.replace('__webpack_public_path__ +','').replace(/\"([a-f0-9]{32})(\.png)/, '\"'+this.options.cdn+'/$1$2');
         }
-                return res;
-
+        return res;
     }
-
 }
 
 module.exports.raw = true;
-
